faee2fbcd8f8678d0dc52040e46e0f8d09d0686a
이세령 <juniorryeong@gmail.com>
2022-11-10 16:01:58 +0900

    등록/수정/삭제 기능 구현
    - Mustache 문법 : data 인덱싱 {{}}
    - button id -> index.js onclick 이벤트 -> ajax 로 주소값 매핑 -> controller -> service -> repository
    - index.js : 유효범위(scope)를 활용한 function 선언함으로써 각 스코프 별 init, save 등과 같은 기본적인 함수명의 중복을 막을 수 있다

--- src/main/java/com/study/book/service/posts/PostsService.java
+++ src/main/java/com/study/book/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.study.book.service.posts;
 
 import com.study.book.domain.posts.Posts;
 import com.study.book.domain.posts.PostsRepository;
+import com.study.book.web.Dto.PostsListResponseDto;
 import com.study.book.web.Dto.PostsResponseDto;
 import com.study.book.web.Dto.PostsSaveRequestDto;
 import com.study.book.web.Dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -28,10 +32,24 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true) //readOnly=true : 트랜잭션 범위는 유지하되 조회기능만 남겨두어 조회 속도 개선
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)//posts -> new PostsListResponseDto(posts)
+                .collect(Collectors.toList());
+    }
+
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
